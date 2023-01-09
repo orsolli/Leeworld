fabc0f7e331e00ba51667f3615a3c910b541b72c
Ørjan Solli <orjansolli@outlook.com>
2023-01-09 22:38:12 +0100

    Add CSG plugin

---
+++ Assets/Plugins/Node_Editor/Net3dBool/CSGGameObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using Net3dBool;
+using UnityEngine;
+
+public class CSGGameObject
+{
+
+    public static void GenerateMesh(GameObject go, Material ObjMaterial, Solid mesh)
+    {
+
+        var verts = mesh.getVertices();
+        int mlen = verts.Length;
+        var colors = mesh.getColors();
+        int clen = colors.Length;
+
+        MeshFilter mf = go.GetComponent<MeshFilter>();
+        if (mf == null)
+            mf = go.AddComponent<MeshFilter>();
+
+        Mesh tmesh = new Mesh();
+        Vector3[] vertices = new Vector3[mlen];
+        for (int i = 0; i < mlen; i++)
+        {
+            Net3dBool.Point3d p = verts[i];
+            vertices[i] = new Vector3((float)p.x, (float)p.y, (float)p.z);
+        }
+        tmesh.vertices = vertices;
+        tmesh.triangles = mesh.getIndices();
+        Color[] clrs = new Color[clen];
+        for (int j = 0; j < clen; j++)
+        {
+            Net3dBool.Color3f c = colors[j];
+            clrs[j] = new Color((float)c.r, (float)c.g, (float)c.b);
+        }
+        tmesh.colors = clrs;
+        tmesh.RecalculateNormals();
+        mf.mesh = tmesh;
+
+        MeshRenderer mr = go.GetComponent<MeshRenderer>();
+        if (mr == null) mr = go.AddComponent<MeshRenderer>();
+        mr.sharedMaterials = new Material[1];
+        mr.sharedMaterials[0] = ObjMaterial;
+        mr.sharedMaterial = ObjMaterial;
+    }
+}
---
+++ Assets/Plugins/Node_Editor/Nodes/Effect/WarpNode.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using NodeEditorFramework;
+
+[Node (false, "Effect/Warp Node", false)]
+public class WarpNode : Node 
+{
+	public enum WarpType { ShearX, ShearY, ShearZ }
+	public WarpType type = WarpType.ShearX;
+	public float strength = 1.0f;
+
+	public const string ID = "warpNode";
+	public override string GetID { get { return ID; } }
+	
+	public override Node Create (Vector2 pos) 
+	{
+		WarpNode node = CreateInstance<WarpNode> ();
+		
+		node.rect = new Rect (pos.x, pos.y, 200, 100);
+		node.name = "Warp Node";
+		
+		node.CreateInput ("Value", "GameObject");
+		node.CreateOutput ("Output Obj", "GameObject");
+		
+		return node;
+	}
+	
+	public override void NodeGUI () 
+	{
+		UnityEditor.EditorGUIUtility.labelWidth = 100;
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.BeginVertical ();
+
+		Inputs [0].DisplayLayout ();
+
+		GUILayout.EndVertical ();
+		GUILayout.BeginVertical ();
+		
+		Outputs [0].DisplayLayout ();
+		
+		GUILayout.EndVertical ();
+		GUILayout.EndHorizontal ();
+
+		type = (WarpType)UnityEditor.EditorGUILayout.EnumPopup (
+			new GUIContent ("CSG Operation", "The type of warp performed on Input 1"), type);
+		strength = UnityEditor.EditorGUILayout.FloatField ("Strength", strength);
+
+		if (GUI.changed)
+			NodeEditor.RecalculateFrom (this);
+	}
+	
+	public override bool Calculate () 
+	{
+		if (!allInputsReady ())
+			return false;
+		Outputs[0].SetValue<GameObject> (Inputs[0].connection.GetValue<GameObject> ());
+		return true;
+	}
+}
