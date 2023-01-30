661d59be2a960dda9bf49c645426d00cdf403826
Ørjan Solli <orjansolli@outlook.com>
2023-01-30 19:44:06 +0100

    Implement Build class Build from comand-line: `Unity -batchmode -quit -projectPath $(pwd) -executeMethod Leeworld.Builder.BuildProject`

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+
+namespace Leeworld
+{
+    public static class Builder
+    {
+        public static void BuildProject()
+        {
+            var options = new BuildPlayerOptions
+            {
+                scenes = new[] { "Assets/Scenes/MainMenu.unity", "Assets/Scenes/OpenWorld.unity" },
+                target = BuildTarget.WebGL,
+                locationPathName = "Build/WebGL",
+            };
+
+            BuildPipeline.BuildPlayer(options);
+        }
+    }
+}
